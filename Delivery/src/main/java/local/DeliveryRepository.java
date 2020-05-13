bdd433aa044d952bc19b0cb13cd2d2f01ef98622
hangukjin <dlalsk1@naver.com>
2020-05-13 16:33:58 +0900

    init

--- Delivery/src/main/java/local/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package local;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
-
-
-}
